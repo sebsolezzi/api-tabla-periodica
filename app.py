25aabebe15e1fc13af402e593b664d6035f91bfc
sebsolezzi <ssolezzi@gmail.com>
2022-03-23 22:12:13 -0300

    cuarto comit cors

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from routes.elementos import elementos
+from flask_cors import CORS,cross_origin
 
 app = Flask('__name__')
 
 app.register_blueprint(elementos)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.errorhandler(404)
 def error(error):
--- routes/elementos.py
+++ routes/elementos.py
@@ -1,20 +1,24 @@
 from flask import Blueprint,render_template,jsonify
 from funciones import obtener_por_electrones, obtener_por_serie, obtener_por_simbolo, obtener_todos
+from flask_cors import CORS,cross_origin
 
 elementos = Blueprint('elementos',__name__)
 
 @elementos.route('/')
+@cross_origin()
 def render_index():
     return render_template('index.html')
 
 #regrese un json con todos los elementos de tabla periodica
 @elementos.route('/todos')
+@cross_origin()
 def return_elements():
     lista = obtener_todos()
     return jsonify(lista),200
 
 #regresa un json con elemento de tabla que sean de esa serie
 @elementos.route('/serie/<nombreserie>')
+@cross_origin()
 def return_por_Serie(nombreserie):
     if nombreserie == '':
         return jsonify('sin nombre serie'),404
@@ -26,6 +30,7 @@ def return_por_Serie(nombreserie):
 
 #regresa un json con elemento de tabla que tenga esa cantidad de electrones
 @elementos.route('/electrones/<int:electrones>')
+@cross_origin()
 def return_por_electrones(electrones):
     lista = obtener_por_electrones(electrones)
     if len(lista) >= 1:
@@ -36,6 +41,7 @@ def return_por_electrones(electrones):
 
 #regresa un json con el elemento de la tabla que tenga ese simbolo
 @elementos.route('/simbolo/<simbol>')
+@cross_origin()
 def return_por_simbolo(simbol):
     lista = obtener_por_simbolo(simbol)
     if len(lista) >= 1:
